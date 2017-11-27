790d4cf384d6072fb79040a69a9ac7dd84723951
Gabriel Felix <crowleyfelix@gmail.com>
2017-11-27 21:03:00 -0200

    Implementa serviço de merchant

---
+++ affiliation_sdk/services/merchant.py
@@ -0,0 +1,31 @@
+from affiliation_sdk.services.service import Service, CONTEXTS
+from affiliation_sdk.models.operator import Comparison
+
+
+BASE_PATH = "{}/merchant".format(CONTEXTS["merchant"])
+
+ENDPOINTS = {
+    "get": "GetBasicMerchant",
+    "list": "ListMerchants"
+}
+
+URLS = {
+    "get": "{}/{}/".format(BASE_PATH, ENDPOINTS["get"]),
+    "list": "{}/{}/".format(BASE_PATH, ENDPOINTS["list"])
+}
+
+
+class Merchant(Service):
+    def get_by_stonecode(self, stonecode):
+        query = {
+            "ConditionList": [
+                self.build_condition("StoneCode", stonecode,
+                                     Comparison.EQUALS.value)
+            ]
+        }
+        return self.list(query)
+
+    def list(self, query=None):
+        data = self._base_data(ENDPOINTS["list"])
+        data["QueryExpression"] = query
+        return self._request(self.build_url(URLS["list"]), data)
