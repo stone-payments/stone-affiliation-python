7f0c27e19fd654bdaa3b1080214b7bd8aceea6b9
Fadel <pfadel@stone.com.br>
2017-11-29 17:35:22 -0200

    :lipstick: remove espaços em branco

--- stone_affiliation/services/service.py
+++ stone_affiliation/services/service.py
@@ -30,20 +30,20 @@ class Service(object):
 
     def _request(self, url, data):
         LOGGER.info("Requesting to %s", url)
-        
+
         return self._process_response(request.post(url, json=data))
 
     def _process_response(self, response):
         LOGGER.debug("Processing response")
-        
+
         return processors.track_error(response).json()
 
     def build_condition(self, field, value, comparison_operator):
         LOGGER.info("Building condition to Field: %s; Value: %s", field, value)
-        
+
         if not isinstance(comparison_operator, Comparison):
             LOGGER.info("Invalid comparison sent: {}".format(comparison_operator))
-            
+
             raise TypeError(
                 "comparison_operator should be an Comparator Enum")
 
@@ -56,19 +56,19 @@ class Service(object):
 
     def build_url(self, resource_path):
         LOGGER.debug("Building url to resource %s", resource_path)
-        
+
         return "{}{}".format(self.api_url, resource_path)
 
     def _base_data(self, endpoint):
         LOGGER.debug("Building base data to endpoint %s", endpoint)
-        
+
         return {
             "UserCredential": self._build_credentials(endpoint)
         }
 
     def _build_credentials(self, endpoint):
         LOGGER.debug("Building credentials to endpoint %s", endpoint)
-        
+
         return {
             "EffectiveUserId": self.user_email,
             "Signature": self._build_signature(endpoint),
