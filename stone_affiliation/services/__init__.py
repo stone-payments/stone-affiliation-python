b080ca89f9e3eff4e74981a740ba4611531a3a36
Gabriel Lima <gabrielcastroslima@gmail.com>
2018-03-08 10:55:47 -0300

    Altera import

--- stone_affiliation/services/__init__.py
+++ stone_affiliation/services/__init__.py
@@ -6,4 +6,4 @@ Pacote contendo serviços por contexto
 
 from .merchant import Merchant
 from .bank_account import BankAccount
-from .bascic_merchant import BasicMerchant
+from .basic_merchant import BasicMerchant
