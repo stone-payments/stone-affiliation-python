2c3fdb0d1f673a2f725446349cacc8f7080fe616
Gabriel Lima <gabrielcastroslima@gmail.com>
2018-03-08 09:34:06 -0300

    Implementa testes de integração

---
+++ tests/integration/services/test_basic_merchant.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from tests.integration.services.suite import TestSuite
+from stone_affiliation.services import BasicMerchant
+
+
+class TestBankAccount(TestSuite):
+    """
+    TestBankAccount testa a integração da classe de serviço BankAccount
+    com a API de credenciamento
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.service = BasicMerchant(**self.get_config())
+
+    def test_get_by_stonecode(self):
+        resp = self.service.get_by_stonecode(self.merchant["StoneCode"])
+        self.assertEquals(
+            resp["Merchant"]["AffiliationCode"],
+            self.merchant["StoneCode"])
+
+    def test_get_by_merchant_id(self):
+        resp = self.service.get_by_merchant_id(self.merchant["Id"])
+        self.assertEquals(
+            resp["Merchant"]["Id"],
+            self.merchant["Id"])
