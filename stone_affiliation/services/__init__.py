86718dd87e051febb5d83a89a1f4bb5c0db6d8d2
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-04-07 14:58:21 -0300

    import ListTerminalDevices in __init__

--- stone_affiliation/services/__init__.py
+++ stone_affiliation/services/__init__.py
@@ -7,3 +7,4 @@ Pacote contendo serviços por contexto
 from .merchant import Merchant
 from .bank_account import BankAccount
 from .basic_merchant import BasicMerchant
+from .list_terminal_devices import ListTerminalDevices
