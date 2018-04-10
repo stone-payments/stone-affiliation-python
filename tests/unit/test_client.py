36e2a0926a6f8248e024d84c68152c3d22471639
Gabriel Lima <gabrielcastroslima@gmail.com>
2018-04-10 15:54:50 -0300

    Test service classes

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -3,7 +3,8 @@
 from mock import patch
 from tests.suite import TestSuite
 from stone_affiliation import StoneAffiliation
-from stone_affiliation.services import (Merchant, BankAccount)
+from stone_affiliation.services import (Merchant, BankAccount, BasicMerchant,
+                                        ListPagedTerminalDevices, ListTerminalDevices)
 
 MOCK_CONFIG = {
     "api_url": "api_url",
@@ -18,7 +19,7 @@ MOCK_USER = {
 
 class TestStoneAffiliation(TestSuite):
     """
-    TestStoneAffiliation testa unitariamente a classe 
+    TestStoneAffiliation testa unitariamente a classe
     agregadora de serviços StoneAffiliation
     """
 
@@ -32,3 +33,15 @@ class TestStoneAffiliation(TestSuite):
     def test_bank_account_service(self):
         actual = self.client.bank_account_service("user_email", "source_ip")
         self.assertTrue(isinstance(actual, BankAccount))
+
+    def test_basic_merchant_service(self):
+        actual = self.client.basic_merchant_service("user_email", "source_ip")
+        self.assertTrue(isinstance(actual, BasicMerchant))
+
+    def test_list_paged_terminal_service(self):
+        actual = self.client.list_paged_terminal_devices("user_email", "source_ip")
+        self.assertTrue(isinstance(actual, ListPagedTerminalDevices))
+
+    def test_list_terminal_service(self):
+        actual = self.client.list_terminal_devices("user_email", "source_ip")
+        self.assertTrue(isinstance(actual, ListTerminalDevices))
