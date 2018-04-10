cd9a49aa81a56fbbe9b1ee91ec175d17ebf9d42d
Gabriel Lima <gabrielcastroslima@gmail.com>
2018-04-10 15:58:44 -0300

    Fix test function name

--- tests/unit/services/test_basic_merchant.py
+++ tests/unit/services/test_basic_merchant.py
@@ -12,7 +12,7 @@ class TestBasicMerchant(TestSuite):
 
     def setUp(self):
         self.service = BasicMerchant("api_url", "app_key", "secret_key",
-                                "source_ip", "user_email")
+                                     "source_ip", "user_email")
 
     @patch("stone_affiliation.services.service.Service._base_data",
            return_value={})
@@ -21,33 +21,32 @@ class TestBasicMerchant(TestSuite):
     @patch("stone_affiliation.services.service.Service._request",
            return_value="response")
     def test_get_by_stonecode(self, mock_requester, mock_url_builder,
-                  mock_data_builder):
+                              mock_data_builder):
         actual = self.service.get_by_stonecode("516165165")
 
         mock_data_builder.assert_called_once_with("GetBasicMerchant")
         mock_url_builder.assert_called_once_with(
             "/Merchant/MerchantService.svc/merchant/GetBasicMerchant/")
         mock_requester.assert_called_once_with("url_builded", {
-            "StoneCode": "516165165"            
+            "StoneCode": "516165165"
         })
         self.assertEqual(actual, 'response')
 
-        
+
     @patch("stone_affiliation.services.service.Service._base_data",
            return_value={})
     @patch("stone_affiliation.services.service.Service.build_url",
            return_value="url_builded")
     @patch("stone_affiliation.services.service.Service._request",
            return_value="response")
-    def test_get_by_stonecode(self, mock_requester, mock_url_builder,
-                  mock_data_builder):
+    def test_get_by_merchant_id(self, mock_requester, mock_url_builder,
+                                mock_data_builder):
         actual = self.service.get_by_merchant_id("5569")
 
         mock_data_builder.assert_called_once_with("GetBasicMerchant")
         mock_url_builder.assert_called_once_with(
             "/Merchant/MerchantService.svc/merchant/GetBasicMerchant/")
         mock_requester.assert_called_once_with("url_builded", {
-            "MerchantId": "5569"            
+            "MerchantId": "5569"
         })
         self.assertEqual(actual, 'response')
-
