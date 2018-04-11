fdbc1a4ff30b5d5c0d9bef8806b896c22642e8ee
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-04-11 12:19:17 -0300

    add ListTerminalDevices and ListPagedTerminalDevices integrations tests in test_merchant

--- tests/integration/services/test_merchant.py
+++ tests/integration/services/test_merchant.py
@@ -31,3 +31,15 @@ class TestMerchant(TestSuite):
         resp = self.service.get_by_stonecodes(
             [merchant["StoneCode"] for merchant in self.merchants])
         self.assertEqual(len(resp["ListedMerchants"]), len(self.merchants))
+
+    def test_get_list_paged_terminal_devices_by_stonecode(self):
+        resp = self.service.get_list_paged_terminal_devices_by_stonecode(self.merchant["StoneCode"])
+        terminal_list = resp["ListedTerminalDevices"]
+        min_terminal = 0
+        self.assertTrue(len(terminal_list) > min_terminal)
+
+    def test_get_list_terminal_devices_by_merchant_id(self):
+        merchant_id = 10168
+        min_terminal_device = 0
+        resp = self.service.get_list_terminal_devices_by_merchant_id(merchant_id)
+        self.assertTrue(len(resp["TerminalList"]) > min_terminal_device)
