246d3672f2831f201333f410cd21d907f3201177
Gabriel Lima <gabrielcastroslima@gmail.com>
2018-04-11 16:28:15 -0300

    Fix tests imports after file refactor

---
+++ tests/integration/services/test_terminal.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from tests.integration.services.suite import TestSuite
+from stone_affiliation.services import Terminal
+
+
+class TestMerchant(TestSuite):
+    """
+    TestTerminal testa a integração da classe de serviço Terminal
+    com a API de credenciamento
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.service = Terminal(**self.get_config())
+
+    def test_get_list_paged_terminal_devices_by_stonecode(self):
+        resp = self.service.get_by_stonecode(self.merchant["StoneCode"])
+        terminal_list = resp["ListedTerminalDevices"]
+        min_terminal = 0
+        self.assertTrue(len(terminal_list) > min_terminal)
+
+    def test_get_list_terminal_devices_by_merchant_id(self):
+        merchant_id = 10168
+        min_terminal_device = 0
+        resp = self.service.get_by_merchant_id(merchant_id)
+        self.assertTrue(len(resp["TerminalList"]) > min_terminal_device)
--- tests/unit/services/test_merchant.py
+++ tests/unit/services/test_merchant.py
@@ -93,26 +93,3 @@ class TestMerchant(TestSuite):
             }
         })
         self.assertEqual(actual, 'response')
-
-    @patch("stone_affiliation.services.service.Service._base_data", return_value={})
-    @patch("stone_affiliation.services.service.Service.build_url", return_value="url_builded")
-    @patch("stone_affiliation.services.service.Service._request", return_value="response")
-    def test_get_list_paged_terminal_devices_by_stonecode(self, mock_requester, mock_url_builder, mock_data_builder):
-        actual = self.service.get_list_paged_terminal_devices_by_stonecode("919243797")
-
-        mock_data_builder.assert_called_once_with("ListPagedTerminalDevices")
-        mock_url_builder.assert_called_once_with("/Merchant/MerchantService.svc/merchant/ListPagedTerminalDevices/")
-        self.assertEqual(actual, 'response')
-
-    @patch("stone_affiliation.services.service.Service._base_data", return_value={})
-    @patch("stone_affiliation.services.service.Service.build_url", return_value="url_builded")
-    @patch("stone_affiliation.services.service.Service._request", return_value="response")
-    def test_get_list_terminal_devices_by_merchant_id(self, mock_requester, mock_url_builder, mock_data_builder):
-        actual = self.service.get_list_terminal_devices_by_merchant_id("516165165")
-
-        mock_data_builder.assert_called_once_with("ListTerminalDevices")
-        mock_url_builder.assert_called_once_with("/Merchant/MerchantService.svc/merchant/ListTerminalDevices/")
-        mock_requester.assert_called_once_with("url_builded", {
-            "MerchantId": "516165165"
-        })
-        self.assertEqual(actual, 'response')
