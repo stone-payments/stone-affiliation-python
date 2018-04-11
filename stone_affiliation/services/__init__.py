cc0960ccc8fac21cf062c05a7c5c44a442d88729
JGabrielFreitas <jgabrielafreitas@gmail.com>
2018-04-11 12:21:29 -0300

    remove useless imports from __init__

--- stone_affiliation/services/__init__.py
+++ stone_affiliation/services/__init__.py
@@ -7,5 +7,3 @@ Pacote contendo serviços por contexto
 from .merchant import Merchant
 from .bank_account import BankAccount
 from .basic_merchant import BasicMerchant
-from .list_terminal_devices import ListTerminalDevices
-from .list_paged_terminal_devices import ListPagedTerminalDevices
